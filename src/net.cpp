e04619d46e981f41560b2e06cb8d8efcf6adfb3c
dmitriy79 <sinoptik79@inbox.ru>
2026-06-28 03:52:30 +1000

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -49,7 +49,7 @@ uint64 nLocalServices = (fClient ? 0 : NODE_NETWORK);
 CAddress addrLocalHost(CService("0.0.0.0", 0), nLocalServices);
 static CNode* pnodeLocalHost = NULL;
 uint64 nLocalHostNonce = 0;
-array<int, THREAD_MAX> vnThreadsRunning;
+boost::array<int, THREAD_MAX> vnThreadsRunning;
 static SOCKET hListenSocket = INVALID_SOCKET;
 CAddrMan addrman;
 
