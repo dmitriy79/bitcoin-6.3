e4213abeb2fd9c4192ae9d96be6acf74172aa5cb
dmitriy79 <sinoptik79@inbox.ru>
2026-09-11 13:32:37 +1000

    Add files via upload

--- src/version.h
+++ src/version.h
@@ -1,19 +1,12 @@
-// Copyright (c) 2012 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_VERSION_H
 #define BITCOIN_VERSION_H
 
 #include <string>
 
-//
-// client versioning
-//
-
-// These need to be macro's, as version.cpp's voodoo requires it
+// Принудительно завышаем версию клиента для прохождения проверок сети
 #define CLIENT_VERSION_MAJOR       0
-#define CLIENT_VERSION_MINOR       6
-#define CLIENT_VERSION_REVISION    3
+#define CLIENT_VERSION_MINOR       21
+#define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
 static const int CLIENT_VERSION =
@@ -26,24 +19,21 @@ extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
 extern const std::string CLIENT_DATE;
 
-//
-// network protocol versioning
-//
+// Актуальная версия сетевого протокола
+static const int PROTOCOL_VERSION = 70016;
 
-static const int PROTOCOL_VERSION = 60001;
+// ПОДНИМАЕМ МИНИМАЛЬНЫЕ ВЕРСИИ ДЛЯ СОВМЕСТИМОСТИ:
+// Разрешаем подключаться только к узлам с современным протоколом
+static const int MIN_PROTO_VERSION = 70002;
 
-// earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
-
-// nTime field added to CAddress, starting with this version;
-// if possible, avoid requesting addresses nodes older than this
+// Время в CAddress поддерживается всеми современными нодами
 static const int CADDR_TIME_VERSION = 31402;
 
-// only request blocks from nodes outside this range of versions
+// Диапазоны версий без поддержки блоков (оставляем как в оригинале)
 static const int NOBLKS_VERSION_START = 32000;
 static const int NOBLKS_VERSION_END = 32400;
 
-// BIP 0031, pong message, is enabled for all versions AFTER this one
+// Сообщения PONG (BIP 0031) гарантированно включены
 static const int BIP0031_VERSION = 60000;
 
-#endif
+#endif // BITCOIN_VERSION_H
